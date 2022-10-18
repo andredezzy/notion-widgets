eee92bf3f0390629ac232e82038a4b571208db46
André "Dezzy" Victor <hello@andredezzy.com>
2022-10-17 22:52:31 -0300

    feat: Add theme switcher to Storybook

--- .storybook/preview.js
+++ .storybook/preview.js
@@ -1,4 +1,7 @@
-import { themes } from '@storybook/theming'
+import React from 'react';
+import { DocsContainer } from '@storybook/addon-docs';
+import { themes } from '@storybook/theming';
+import { useDarkMode } from 'storybook-dark-mode';
 
 import '../src/styles/global.css';
 
@@ -10,7 +13,23 @@ export const parameters = {
       date: /Date$/,
     },
   },
-  docs: {
-    theme: themes.dark,
+  darkMode: {
+    current: 'dark',
+    dark: { ...themes.dark },
+    light: { ...themes.normal }
   },
+  docs: {
+		container: props => {
+			const isDark = useDarkMode();
+
+  		const { id: storyId, storyById } = props.context;
+      const {
+        parameters: { docs = {} },
+      } = storyById(storyId);
+      
+      docs.theme = isDark ? themes.dark : themes.light;
+
+      return React.createElement(DocsContainer, props);
+		},
+	},
 }
